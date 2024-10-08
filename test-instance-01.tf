92ea49bc8b730f5ca3ce783d5a0f78b4adebff6d
bhatgaur <grv@outlook.in>
2024-10-08 10:57:23 +0200

    add test-user-01.tf

--- test-instance-01.tf
+++ test-instance-01.tf
@@ -1,19 +1,4 @@
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-}
-
-provider "aws" {
-  region     = "eu-central-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
+/*
 resource "aws_instance" "test-instance-01"{
     ami             =   "ami-0e6a13e7a5b66ff4d"
     instance_type   =   "t2.micro"
@@ -21,3 +6,4 @@ resource "aws_instance" "test-instance-01"{
       Name = "test-instance-01"
   }
 }
+*/
---
+++ test-user-01.tf
@@ -0,0 +1,5 @@
+/*
+resource "aws_iam_user" "test-user-01" {
+    name = "test-user-01"
+}
+*/
