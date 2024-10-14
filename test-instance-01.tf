3f317d7bd1c31eb0ef0e071e9a2f22f63d13679a
bhatgaur <grv@outlook.in>
2024-10-14 09:42:36 +0200

    add test-security-group-01.tf

--- test-instance-01.tf
+++ test-instance-01.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_instance" "test-instance-01"{
     ami             =   "ami-0e6a13e7a5b66ff4d"
     instance_type   =   "t2.micro"
@@ -5,3 +6,4 @@ resource "aws_instance" "test-instance-01"{
       Name = "test-instance-01"
   }
 }
+*/
--- test-repo-01.tf
+++ test-repo-01.tf
@@ -1,6 +1,8 @@
+/*
 resource "github_repository" "test-repo-01" {
   name        = "test-repo-01"
   description = "tf"
 
   visibility = "public"
 }
+*/
